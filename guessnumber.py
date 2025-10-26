6cbea76634cbdb624e3e8f7f35b348dd08ef8ae3
Samir Sapkota <samirsapkota29@gmail.com>
2025-10-26 19:28:02 +0000

    Added 2 python projects

---
+++ guessnumber.py
@@ -0,0 +1,16 @@
+import random
+import sys
+num=random.randrange(1,10)
+count=0
+while count<3:
+    prediction=int(input('Enter the predicted numer\n'))
+    if (prediction==0):
+        print('Enter the valid number between 1 and 10')
+        sys.exit()
+    if (prediction==num):
+        print('Your prediction is correct')
+        sys.exit()
+    elif(prediction>num):
+        print('Your prediction is greater than number')
+    else:
+        print('Your prediction is less than number')
